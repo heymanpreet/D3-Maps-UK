c2701ef187925407daccda6620e3b8c2d93ac898
heymanpreet <manpreet.singh.chawla94@gmail.com>
2021-11-09 10:45:06 +0000

    Fixes for Tooltip and layout

--- scripts/d3-assn.js
+++ scripts/d3-assn.js
@@ -1,33 +1,25 @@
-var w = 1300;
-var h = window.screen.height;
+var screenWidth = 1300;
+var screenHeight = window.screen.height;
 var svg;
-var circleLabels;
 
-var towns = document.querySelector(".range").value;
+// Range Slider
+const rangeInput = document.querySelector(".range-wrap .range");
+let rangeValue = document.querySelector(".range-wrap .value div");
 
-const allRanges = document.querySelectorAll(".range-wrap");
-allRanges.forEach(wrap => {
-    const range = wrap.querySelector(".range");
-    const bubble = wrap.querySelector(".bubble");
+let start = parseFloat(rangeInput.min);
+let end = parseFloat(rangeInput.max);
+let step = parseFloat(rangeInput.step);
 
-    range.addEventListener("input", () => {
-        setBubble(range, bubble);
-    });
-    setBubble(range, bubble);
+for (let i = start; i <= end; i += step) {
+    rangeValue.innerHTML += '<div>' + i + '</div>';
+}
+rangeInput.addEventListener("input", function () {
+    let top = parseFloat(rangeInput.value) / step * -40;
+    rangeValue.style.marginTop = top + "px";
 });
 
-function setBubble(range, bubble) {
-    const val = range.value;
-    const min = range.min;
-    const max = range.max;
-    const newVal = Number(((val - min) * 100) / (max - min));
-    bubble.innerHTML = val;
-
-    bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
-}
-
 // Creating Projection of Map with customizations
-var projection = d3.geoAlbers().center([1.5, 55.2]).rotate([4.4, 0]).parallels([50, 50]).scale(5000).translate([w / 2, h / 2]);
+var projection = d3.geoAlbers().center([1.5, 55.2]).rotate([4.4, 0]).parallels([50, 50]).scale(5000).translate([screenWidth / 2, screenHeight / 2]);
 var path = d3.geoPath().projection(projection);
 
 // load data on Page Load
@@ -56,9 +48,9 @@ function loadRegionNewData() {
 // Creating SVG of Map
 function createSvg() {
     svg = d3.select("div#content").append("svg").style("background-color", "#c9e8fd")
-        .attr("viewBox", "0 0 " + w + " " + h)
+        .attr("viewBox", "0 0 " + screenWidth + " " + screenHeight)
         .classed("svg-content", true)
-    
+
     svg
         .transition()
         .duration(700)
@@ -82,29 +74,33 @@ function drawCircles(mapData, dataPoints) {
         document.getElementById("tooltip1").remove();
     }
     var Tooltip = d3.select("div#main-content .info")
-        .append("div")
+        // .append("div")
         .attr("class", "tooltip")
         .attr("id", "tooltip1")
         .style("opacity", 1)
-        .style("background-color", "white")
+        .style("background-color", "paleturquoise")
         .style("border", "solid")
         .style("border-width", "2px")
         .style("border-radius", "5px")
         .style("padding", "5px")
+        .style("box-shadow","rgb(0 0 0 / 25%) 0px 20px 40px -14px")
 
     // Showing Region Details of Hover
     const mouseover = function (event, d) {
-        Tooltip.style("opacity", 1)
+        Tooltip.append("div")
+            .style("opacity", 1)
+            .style("transition", "all .5s ease")
+
     }
     var mousemove = function (event, d) {
         Tooltip
-            .html(d.County + "<br>" + "Population: " + d.Population +"<br>" + "Longitude: " + d.lng + "<br>" + "Latitude: " + d.lat)
+            .html("<b>" + d.County + "<b>" + "<br>" + "Population: " + d.Population + "<br>" + "Longitude: " + d.lng + "<br>" + "Latitude: " + d.lat)
             .style("left", (event.x) / 2 + "px")
             .style("top", (event.y) / 2 - 30 + "px")
     }
-    var mouseleave = function (event, d) {
-        Tooltip.style("opacity", 0)
-    }
+    // var mouseleave = function (event, d) {
+    //     Tooltip.style("opacity", 0)
+    // }
     let g = svg.append("g");
 
     g.selectAll("circle")
@@ -113,12 +109,10 @@ function drawCircles(mapData, dataPoints) {
         .append("circle")
         .on("mouseover", mouseover)
         .on("mousemove", mousemove)
-        .on("mouseleave", mouseleave)
+        // .on("mouseleave", mouseleave)
         .transition()
         .duration(3000)
         .attr("cx", function (d) {
-            this.circleLabels = d;
-            console.log(this.circleLabels);
             return projection([d.lng, d.lat])[0] + 5;
         })
         .attr("cy", function (d) {
